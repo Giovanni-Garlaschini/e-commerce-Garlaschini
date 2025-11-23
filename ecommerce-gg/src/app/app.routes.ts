b4c23f9feaa438faa61bf5d7015e1db9b5051f9c
Giovanni-Garlaschini <garlaschinigiovanni@gmail.com>
2025-11-23 19:29:20 +0100

    Feat: rotte create e pagine collegate

--- ecommerce-gg/src/app/app.routes.ts
+++ ecommerce-gg/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./pages/home/home').then(m => m.HomePage)
+  },
+  {
+    path: 'shop',
+    loadComponent: () => import('./pages/shop/shop').then(m => m.ShopPage)
+  },
+  {
+    path: 'contatti',
+    loadComponent: () => import('./pages/contatti/contatti').then(m => m.ContattiPage)
+  }
+];
---
+++ ecommerce-gg/src/app/pages/contatti/contatti.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contatti',
+  standalone: true,
+  templateUrl: './contatti.html',
+  styleUrls: ['./contatti.css']
+})
+export class ContattiPage {}
